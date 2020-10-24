6337095945256597ddd7954c5b9b40d1cf026c07
LoQeN00 <LoQeN00@o2.pl>
2020-10-24 03:20:57 +0200

    Naura

--- routes/dashboardRouter.js
+++ routes/dashboardRouter.js
@@ -14,24 +14,6 @@ const timeZoneOffset = 3600000 * Math.abs(new Date().getTimezoneOffset())/60
 // achievement
 router.get('/',isNotLoggedIn,async (req,res)=> {
 
-    
-
-    // const findUser = await User.findOne({
-    //     email: req.user.profile._json.email
-    // })
-
-    // if(findUser === null) {
-    //     const user = new User({
-    //         email: req.user.profile._json.email,
-    //         username: req.user.profile.displayName,
-    //         steps: 0,
-    //         created: Date.now() + timeZoneOffset,
-    //         achievements : [],
-    //         lastLoggedIn: Date.now() + timeZoneOffset
-    //     })
-    //     await user.save()
-    // }
-    
     const motivationQuoteData = await getMotivationQuote()
 
     const randomIndex = Math.floor(Math.random()*motivationQuoteData.data.length)
@@ -47,6 +29,22 @@ router.get('/',isNotLoggedIn,async (req,res)=> {
         motivationQuoteAuthor,
         motivationQuoteText,
     })
+
+    const findUser = await User.findOne({
+        email: req.user.profile._json.email
+    })
+
+    if(findUser === null) {
+        const user = new User({
+            email: req.user.profile._json.email,
+            username: req.user.profile.displayName,
+            steps: 0,
+            created: Date.now() + timeZoneOffset,
+            achievements : [],
+            lastLoggedIn: Date.now() + timeZoneOffset
+        })
+        await user.save()
+    }
 })
 
 
