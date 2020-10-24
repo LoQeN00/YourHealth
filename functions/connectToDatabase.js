23f14dd1396ab27e8837e2c29f2e7ff985177bd8
LoQeN00 <LoQeN00@o2.pl>
2020-10-24 02:30:10 +0200

    Naura

--- functions/connectToDatabase.js
+++ functions/connectToDatabase.js
@@ -7,7 +7,7 @@ const connectToDatabase = async (url) => {
             useUnifiedTopology: true,
             useFindAndModify: false,
             useCreateIndex: true
-          },()=>console.log('Succesfully connected to databse'));
+          },()=>console.log('Zmienione connect'));
     } catch(e) {
         console.log(e)
     }
